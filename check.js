94eafe7db7ec709309d6fb86e9c5ffdad6804891
kogutkies <156412377+kogutkies@users.noreply.github.com>
2024-02-17 20:05:46 +0100

    Update check.js

--- check.js
+++ check.js
@@ -3,7 +3,7 @@ function checkifopera()
   console.log(platform);
   if(platfrom.name=="Opera")
   {
-  document.write("<h1>This website doesn't support Opera/GX</h1>");
+  document.write("<h1>This website doesn't support Opera/GX go open it in chrome or something.</h1>");
 setTimeout(function()
 {
   window.location.replace("https://youtu.be/0NTl_SXSgd4?si=b3cUAS1yNpS-kUjl&t=10");
