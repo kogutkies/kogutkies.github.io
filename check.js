62a851e80d5ff24b4f889ee1e1b1c104112a562c
kogutkies <156412377+kogutkies@users.noreply.github.com>
2024-03-12 10:59:57 +0100

    Update check.js

--- check.js
+++ check.js
@@ -1,18 +1,5 @@
 function checkifopera()
 {
-  let userAgentString =  
-                navigator.userAgent; 
-  let operaAgent =  
-          userAgentString.indexOf("OP") > -1; 
-            
-  let chromeAgent =  
-                userAgentString.indexOf("Chrome") > -1; 
-             
-  if ((chromeAgent) && (operaAgent))  
-                chromeAgent = false; 
-  if(operaAgent===true)
-  {
-  window.location.replace("redirect.html");
-}
+console.log("Code Execution Unavailable");
 }
 checkifopera();
